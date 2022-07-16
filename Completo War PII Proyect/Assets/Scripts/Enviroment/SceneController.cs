0c4b0c57c92496af06af6951b6b38e1535742ecf
Benjamín D <benjamin.donoso@alumnos.ucn.cl>
2022-07-16 12:19:05 -0400

    Example

--- Completo War PII Proyect/Assets/Scripts/Enviroment/SceneController.cs
+++ Completo War PII Proyect/Assets/Scripts/Enviroment/SceneController.cs
@@ -4,7 +4,10 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class SceneController : MonoBehaviour
-{
+{   
+
+    
+
     public void ChangeScene(string skin){
         StaticValuesController.skin = skin;
         SceneManager.LoadScene("testLevel0");
