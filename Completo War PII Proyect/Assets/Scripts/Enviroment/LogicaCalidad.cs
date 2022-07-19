a548ac00c68aa1c27050ebf1499efc59295eeddd
Benjamín D <benjamin.donoso@alumnos.ucn.cl>
2022-07-18 23:29:02 -0400

    quick fixes
    fixed navigation in main menu and set scale with screen size in options

--- Completo War PII Proyect/Assets/Scripts/Enviroment/LogicaCalidad.cs
+++ Completo War PII Proyect/Assets/Scripts/Enviroment/LogicaCalidad.cs
@@ -8,18 +8,11 @@ public class LogicaCalidad : MonoBehaviour
     public TMP_Dropdown dropdown;
     public int calidad;
 
-    // Start is called before the first frame update
     void Start()
     {
         calidad = PlayerPrefs.GetInt("numeroDeCalidad", 3);
         dropdown.value = calidad;
         AjustarCalidad();
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
     }
 
     public void AjustarCalidad()
