988e92fc03b2711b89b630a51b15b14771c21f96
Juan Castro <fairyfoxofficial@gmail.com>
2022-07-20 18:51:24 -0400

    Menú finalizado, creo
    tiene las opciones listas, lo de la canción de fondo no sé cómo mantenerlo

--- Completo War PII Proyect/Assets/Scripts/Enviroment/SceneController.cs
+++ Completo War PII Proyect/Assets/Scripts/Enviroment/SceneController.cs
@@ -22,6 +22,11 @@ public class SceneController : MonoBehaviour
         SceneManager.LoadScene("MainMenu");
     }
 
+    public void Exit()
+    {
+        Application.Quit();
+    }
+
     void Update() {
         if(Input.GetKeyDown(KeyCode.Escape) && SceneManager.GetActiveScene().name != "Menu"){
             SceneManager.LoadScene("Menu");
