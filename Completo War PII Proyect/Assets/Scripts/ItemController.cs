fd06236dd89801095fa968dd161607c0bea5be28
Benjamín D <benjamin.donoso@alumnos.ucn.cl>
2022-07-21 05:14:01 -0400

    items heal
    might change to get the item and then use it but for now thats it

--- Completo War PII Proyect/Assets/Scripts/ItemController.cs
+++ Completo War PII Proyect/Assets/Scripts/ItemController.cs
@@ -8,6 +8,7 @@ public class ItemController : MonoBehaviour
 {
     [SerializeField] private int healingPoints = 1;
     [SerializeField] private bool isHealingBoost = false;
+    [SerializeField] private AudioClip pickClip;
 
     void OnTriggerEnter2D(Collider2D other){
         if(other.gameObject.tag == "Player"){
@@ -17,14 +18,16 @@ public class ItemController : MonoBehaviour
             HealthBar hb = (HealthBar)FindObjectOfType(typeof(HealthBar));
 
             if(player.hp+healingPoints >player.currentMaxHealth){
-                if(isHealingBoost) player.currentMaxHealth++;
+                if(isHealingBoost) player.currentMaxHealth+=2;
+                hb.Heal(player.currentMaxHealth - player.hp);
                 player.hp = player.currentMaxHealth;
             }else{
                 player.hp += healingPoints;
+                hb.Heal(healingPoints);
             }
 
-            hb.Heal(healingPoints);
-            
+            GameObject.FindWithTag("Audio").GetComponent<AudioSource>().PlayOneShot(pickClip, 0.7f);
+
             Destroy(gameObject);
         }
     }
